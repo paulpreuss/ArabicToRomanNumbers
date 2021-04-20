2a2258582143b2ec49fee1e2bdab2e8bad5e7bcb
pp <paulpreuss.private@gmail.com>
2021-04-20 16:01:55 +0200

    the program does not close after an inncorrect input

--- ArabicToRomanNumbers/Program.cs
+++ ArabicToRomanNumbers/Program.cs
@@ -9,8 +9,22 @@ namespace ArabicToRomanNumbers
             while (true)
             {
                 Console.WriteLine("Which number do you want to convert?");
-                int number = Int32.Parse(Console.ReadLine());
-                Console.WriteLine(ArabicToRomanNumbersConverter.Convert(number));
+                try
+                {
+                    int number = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine(ArabicToRomanNumbersConverter.Convert(number));
+                }
+                catch (Exception ex)
+                {
+                    if (ex is ArgumentOutOfRangeException)
+                    {
+                        Console.WriteLine("Please enter a number between 1 and 3999.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a number.");
+                    }
+                }   
             }
         }
     }
